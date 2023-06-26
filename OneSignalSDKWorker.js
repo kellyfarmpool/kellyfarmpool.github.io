9eb4c25dda85de88f2c3b66c95eaabfb28fa8836
kellyfarmpool <kellyfarmpool@gmail.com>
2023-06-26 14:41:51 -0600

    Update OneSignalSDKWorker.js

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -1,6 +1,1 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js');
-
-workbox.routing.registerRoute(
-    ({request}) =>request.destination === 'image',
-    new workbox.strategies.CacheFirst()
-)
+importScripts('https://cdn.onesignal.com/sdks/OneSignalSDKWorker.js');
