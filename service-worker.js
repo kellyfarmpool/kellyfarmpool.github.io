172507a844715ade0d7c27da86e883eaabfab191
kellyfarmpool <kellyfarmpool@gmail.com>
2023-07-18 15:44:32 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,4 @@
+importScripts('https://cdn.onesignal.com/sdks/OneSignalSDKWorker.js');
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js');
 
 workbox.routing.registerRoute(
